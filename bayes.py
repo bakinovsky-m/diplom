494c241c464dc03905110eeed78130279ec0d32e
bakinovsky-m <makbak@ya.ru>
2019-04-22 15:05:18 +0300

    signal switch

--- bayes.py
+++ bayes.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-DELTA = 0.3
+DELTA = 0.2
 
 def prob_up(prev):
     res = abs(prev + abs(prev) * DELTA)
@@ -24,26 +24,46 @@ def prob(p_t):
     p_b = PROB_B_T * p_t + (1 - PROB_B_T) * (1 - p_t)
     return PROB_B_T * p_t / p_b
 
-signals = [1,0,0,0,0,1,1,0,0,0,0,0,0]
-signals += [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-signals += [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+signals = [1,0,0,0,0,1,1,0,0,0]
+signals += [1,1,1,1,1,0,0,1,1,1]
+signals += [1,1,1,1,1,1,1,1,1,1]
+signals += [1,1,1,1,1,1,1,1,1,1]
+signals += [1,1,1,1,1,1,1,1,1,1]
+signals += [0,0,0,0,0,0,0,0,0,0]
+signals += [0,0,0,0,0,0,0,0,0,0]
+signals += [0,0,0,0,0,0,0,0,0,0]
+# signals += [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+# signals += [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+# signals += [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+# signals += [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
-prev_p = 0.1
+prev_p = 0.5
 
 probs = []
 probs_bayes = []
 diffs = []
 
+cur_signal = 1
+counter = 0
+
 for s in signals:
-    if s == 1:
+    # if counter >= 20:
+        # cur_signal = 0
+    if s == cur_signal:
         tmp_p = prob_up(prev_p)
-    elif s == 0:
+    elif s != cur_signal:
         tmp_p = prob_down(prev_p)
     bayes_p = prob(tmp_p)
     diffs.append(abs(tmp_p - prev_p))
     prev_p = tmp_p
     probs.append(prev_p)
     probs_bayes.append(bayes_p)
+    print(counter, "cursign:", bayes_p, cur_signal)
+    if bayes_p <= 0.5:
+        cur_signal = abs(1 - cur_signal)
+        prev_p = 1 - prev_p
+        print(counter, "new:", bayes_p, cur_signal)
+    counter += 1
 
 
 plt.plot(probs, 'go-')
