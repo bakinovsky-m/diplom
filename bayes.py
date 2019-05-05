5da908a8a6314e7541057f6ecf6f5415d43baebb
bakinovsky-m <makbak@ya.ru>
2019-05-05 15:35:58 +0300

    generator

--- bayes.py
+++ bayes.py
@@ -1,25 +1,17 @@
 import matplotlib.pyplot as plt
 
-DELTA = 0.2
+DELTA = 0.8
 
 def prob_up(prev):
     res = prev + prev * DELTA
-    # res = prev + (prev**(1/2))*DELTA
-    # res = abs(prev + abs(prev) * DELTA)
     if res >= 1:
         return 0.99
-    # return abs(prev + abs(prev) * DELTA)
     return res
 
 def prob_down(prev):
     res = prev - (1 - prev) * DELTA
-    # print(prev)
-    # res = prev - (1 - (prev**(1/2)))*(1-DELTA)
-    # if res >= 1:
-        # return 0.99
     if res <= 0:
         return 0.01
-    # return abs(prev - (1 - abs(prev)) * DELTA)
     return res
 
 PROB_B_T = 0.9
@@ -30,43 +22,49 @@ def prob(p_t):
     return PROB_B_T * p_t / p_b
 
 # signals = [1,0,0,0,0,1,1,0,0,1]
-signals = [1,1,1,1,1,1,1,1,1,1]
-signals += [1,1,1,1,1,1,1,1,1,1]
-signals += [0,0,0,0,0,0,0,0,0,0]
-signals += [0,0,0,0,0,0,0,0,0,0]
-signals += [1,1,1,1,1,1,1,1,1,1]
-signals += [1,1,1,1,1,1,1,1,1,1]
-signals += [1,1,1,1,1,1,1,1,1,1]
-signals += [0,0,0,0,0,0,0,0,0,0]
-signals += [0,0,0,0,0,0,0,0,0,0]
-signals += [1,1]
-signals += [0,0,0,0,0,0,0,0,0,0]
-signals += [0,0,0,0,0,0,0,0,0,0]
-signals += [1,1,1,1,1,1]
+# signals = [1,1,1,1,1,1,1,1,1,1]
+# signals += [1,1,1,1,1,1,1,1,1,1]
+# signals += [0,0,0,0,0,0,0,0,0,0]
+# signals += [0,0,0,0,0,0,0,0,0,0]
+# signals += [1,1,1,1,1,1,1,1,1,1]
+# signals += [1,1,1,1,1,1,1,1,1,1]
+# signals += [1,1,1,1,1,1,1,1,1,1]
+# signals += [0,0,0,0,0,0,0,0,0,0]
+# signals += [0,0,0,0,0,0,0,0,0,0]
+# signals += [1,1]
+# signals += [0,0,0,0,0,0,0,0,0,0]
+# signals += [0,0,0,0,0,0,0,0,0,0]
+# signals += [1,1,1,1,1,1]
 # signals += [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 # signals += [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 # signals += [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 # signals += [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+from generator import Generator
+g = Generator(PROB_B_T, 35, (0,1))
+
+signals = []
+realsignals = []
+for _ in range(300):
+    signals.append(g.next())
+    realsignals.append(g.cur_signal)
 
 prev_p = 0.1
 
 probs = []
 probs_bayes = []
-diffs = []
+# diffs = []
 
 cur_signals = []
 cur_signal = 1
 counter = 0
 
 for s in signals:
-    # if counter >= 20:
-        # cur_signal = 0
     if s == cur_signal:
         tmp_p = prob_up(prev_p)
     elif s != cur_signal:
         tmp_p = prob_down(prev_p)
     bayes_p = prob(tmp_p)
-    diffs.append(abs(tmp_p - prev_p))
+    # diffs.append(abs(tmp_p - prev_p))
     prev_p = tmp_p
     probs.append(prev_p)
     probs_bayes.append(bayes_p -0.01)
@@ -78,35 +76,13 @@ for s in signals:
     cur_signals.append(cur_signal + .01)
     counter += 1
 
+plt.figure(figsize=(10, 10))
 plt.xlabel("t")
 plt.ylabel("Уверенность")
-
 plt.plot(probs, 'go-')
 plt.plot(probs_bayes, 'bo-')
 plt.plot(cur_signals, 'ro')
 plt.plot(signals, 'yo')
-# plt.plot(diffs, 'ro-')
+plt.plot(realsignals, 'mo')
 
 plt.show()
-# a = {}
-
-# counter = 0
-# while counter <= 1000:
-#     for i in range(0, 10):
-#         try:
-#             a[i] == None
-#         except KeyError:
-#             a[i] = []
-#         a[i].append(prob(i, random.uniform(0.8, 0.99)))
-#     counter += 1
-
-# qwe = {}
-# for k, v in a.items():
-#     mean = 0
-#     for i in v:
-#         mean += i
-#     mean /= len(v)
-#     qwe[k] = mean
-
-# for k, v in qwe.items():
-#     print(k, ": ", v)
---
+++ generator.py
@@ -0,0 +1,34 @@
+import random as rand
+import itertools
+
+RHO = 0.9
+N = 10
+SIGNALS = [0,1]
+
+class Generator:
+    def __init__(self, rho, n, signals):
+        self.rho = rho
+        self.n = n
+        self.cur_n = 0
+        self.signals = itertools.cycle(signals)
+        self.cur_signal = next(self.signals)
+
+    def next(self):
+        if self.cur_n == self.n:
+            self.cur_signal = next(self.signals)
+            self.cur_n = 0
+        x = rand.random()
+        res = 0
+        if x <= self.rho:
+            res = self.cur_signal
+        else :
+            res = abs(self.cur_signal-1)
+
+        self.cur_n += 1
+        return res
+
+
+if __name__ == '__main__':
+    g = Generator(RHO, N, SIGNALS)
+    for _ in range(20):
+        print(g.next())
