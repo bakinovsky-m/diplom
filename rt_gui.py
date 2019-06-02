96242148be9afe0ec5dc7c8064011a24ae1b8015
bakinovsky-m <makbak@ya.ru>
2019-06-02 21:37:01 +0300

    plot, r/g lights, scores

--- rt_gui.py
+++ rt_gui.py
@@ -1,20 +1,50 @@
 from generator import Generator
 from bayes import *
 from tkinter import *
-import time
+from PIL import Image, ImageTk
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 W_WIDTH = 900
 W_HEIGHT = 900
 
-RUNNING = True
-DELAY = 50
+RUNNING = False
+DELAY = 5
+
+N = 100
+
+GREEN_L = None # PhotoImage(file='green_light.jpg')
+RED_L = None # PhotoImage(file='red_light.jpg')
+
+def levenshteinDistance(s1, s2):
+    if len(s1) > len(s2):
+        s1, s2 = s2, s1
+
+    distances = range(len(s1) + 1)
+    for i2, c2 in enumerate(s2):
+        distances_ = [i2+1]
+        for i1, c1 in enumerate(s1):
+            if c1 == c2:
+                distances_.append(distances[i1])
+            else:
+                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+        distances = distances_
+    return distances[-1]
+
+def hemmingDist(a_list,b_list):
+    res = 0
+    for a,b in zip(a_list, b_list):
+        if a!=b:
+            res += 1
+    return res
 
 class Observer:
     def __init__(self, small_delta, big_delta):
-        self.pred_signal = 0
         self.small_delta = small_delta
         self.big_delta = big_delta
-        self.cur_conf = 0.5
+        self.cur_conf = 0.9
         self.cur_signal = 0
 
     def process(self, signal):
@@ -38,25 +68,69 @@ class Observer:
         self.small_delta = small_delta
         self.big_delta = big_delta
 
-def run(root, obs, gen, pred, real):
+ITER = 0
+
+scores = []
+
+def run(root, obs, gen, pred_label, real_label, stats_label, real_signals, pred_signals):
     if RUNNING:
         signal = gen.next()
-        print('{} {} {}'.format(signal, obs.cur_signal, obs.cur_conf))
-        if gen.cur_signal >= 0.9:
-            real['text'] = "MOVE"
+        if gen.cur_signal == 1:
+            real_label['text'] = "MOVE"
+            real_label['image'] = GREEN_L
         else:
-            real['text'] = "STOP"
+            real_label['text'] = "STOP"
+            real_label['image'] = RED_L
         obs.process(signal)
-        pred['text'] = obs.cur_signal
         if obs.cur_signal == 1:
-            pred['text'] = "MOVE"
+            pred_label['text'] = "MOVE"
+            pred_label['image'] = GREEN_L
         else:
-            pred['text'] = "STOP"
-    root.after(DELAY, run, root, obs, gen, pred, real)
+            pred_label['text'] = "STOP"
+            pred_label['image'] = RED_L
+        # print('{} {} {}'.format(signal, obs.cur_signal, obs.cur_conf))
+        print('----------')
+        global ITER
+        print("ITER", ITER)
+        print("real_label",gen.cur_signal)
+        print("sign",signal)
+        print("pred",obs.cur_signal, obs.cur_conf)
+        print('----------')
 
-def switch_running():
+        real_signals.append(gen.cur_signal)
+        pred_signals.append(obs.cur_signal)
+
+        if ITER % 10 == 0:
+            stats_label['text'] = "iter: " + str(ITER) + "\n"
+            score = hemmingDist(real_signals[-10*N:],pred_signals[-10*N:])/len(real_signals)
+            scores.append(score)
+            stats_label['text'] += "score: {:0.3}".format(score)
+        # real_signals = real_signals[-N*10:]
+        # pred_signals = pred_signals[-N*10:]
+        ITER += 1
+
+    root.after(DELAY, run, root, obs, gen, pred_label, real_label, stats_label, real_signals, pred_signals)
+
+def switch_running(label):
     global RUNNING
+    if RUNNING == True:
+        label['text'] = "Start"
+    else:
+        label['text'] = "Stop"
     RUNNING = not RUNNING
+
+def draw_plot(root, fig):
+    fig.clear()
+    plt.plot(scores)
+    plt.xlabel("Время симуляции")
+    plt.ylabel("mu(T, Q)")
+    # plt.plot(scores)
+    # plt.draw()
+    # plt.pause(0.01)
+    # plt.legend()
+
+    fig.canvas.draw()
+    root.after(DELAY*100, draw_plot, root, fig)
 
 def main():
     root = Tk()
@@ -82,26 +156,45 @@ def main():
     big_delta_scale.set(0.9)
     big_delta_scale.grid(row=2, column=1)
 
+    global GREEN_L, RED_L
+    green_image = Image.open("green_light.jpg")
+    GREEN_L = ImageTk.PhotoImage(green_image)
+    red_image = Image.open("red_light.png")
+    RED_L = ImageTk.PhotoImage(red_image)
+
     real_label = Label(root, text="real")
     real_label.grid(row=0, column=2)
-    real_label2 = Label(root, text="MOVE", width=50)
+    real_label2 = Label(root, image=RED_L, width=150)
     real_label2.grid(row=1, column=2, rowspan=2)
 
     pred_label = Label(root, text="pred")
     pred_label.grid(row=0, column=3)
-    pred_label2 = Label(root, text="STOP", width=50)
+    pred_label2 = Label(root, image=RED_L, width=150)
     pred_label2.grid(row=1, column=3, rowspan=2)
 
-    obs = Observer(small_delta_scale.get(),big_delta_scale.get())
-    gen = Generator(rho_scale.get(), 50, (0,1))
 
-    start_button = Button(root, text="Start", command=lambda:switch_running())
+    obs = Observer(small_delta_scale.get(),big_delta_scale.get())
+    gen = Generator(rho_scale.get(), N, (0,1))
+
+    start_button = Button(root, text="Start", command=lambda:switch_running(start_button))
     start_button.grid(row=3, column=1)
 
-    root.bind("<Return>", lambda ev: switch_running())
+    stats_label = Label(root, text="")
+    stats_label.grid(row=3, column=2, columnspan=2)
+
+    root.bind("<Return>", lambda ev: switch_running(start_button))
     root.bind("r", lambda ev: obs.update_deltas(small_delta_scale.get(), big_delta_scale.get()))
 
-    root.after(0, run, root, obs, gen, pred_label2, real_label2)
+    fig = plt.figure(1, figsize=(10,6))
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    plot_widget = canvas.get_tk_widget()
+    plot_widget.grid(row=4, column=1, rowspan=4)
+    root.after(0, draw_plot, root, fig)
+
+    real_signals = []
+    pred_signals = []
+
+    root.after(0, run, root, obs, gen, pred_label2, real_label2, stats_label, real_signals, pred_signals)
     root.mainloop()
 
 if __name__ == "__main__":
